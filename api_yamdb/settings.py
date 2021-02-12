6d0dad43c052193393395eb87b918d27e6080e69
Артур Гумеров <arturgumerov@yandex.ru>
2021-02-12 19:41:09 +0300

    additional changes pep8

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': ('rest_framework.pagination.'
-                                    'PageNumberPagination'),
+                                 'PageNumberPagination'),
     'PAGE_SIZE': 10,
 }
 
